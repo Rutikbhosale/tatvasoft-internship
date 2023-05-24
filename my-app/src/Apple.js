0fb4cf4fb150e5739fde0c90ac6de90732dc8467
Rutik bhosale <rutikbhosale1772@gmail.com>
2023-05-24 11:40:43 +0530

    day 3

--- my-app/src/Apple.js
+++ my-app/src/Apple.js
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import  Button from "@mui/material/Button";
 export const Apple = () => {
     const Navigate = useNavigate();
     const onHomepageButtonclick = () => {
-        Navigate("/");
+       // Navigate("/");
+       alert("The button has been clicked");
     };
 
     return( 
         <div>
-           <div> Apple page 🍎 </div>
-           <button onclick={onHomepageButtonclick}>Navigate to Home page </button>
+           <div>Apple page 🍎</div>
+           <Button variant="contained"onClick={onHomepageButtonclick}>Hello World</Button>
+        {  /* <button onclick={onHomepageButtonclick}>Navigate to Home page</button>*/}
         </div>
     );
 };
